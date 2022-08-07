e067567680b04caed6d2c5c2b8c1373c1faaff71
pvborgesz <pvbo.lima@discente.ufma.br>
2022-08-07 12:06:38 -0300

    Finalização do projeto.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Main from "../Main";
 import MenuBar from "../MenuBar";
+import Sidebar from "../Sidebar";
 import { Container, Wrapper } from "./styles";
 
 const Layout: React.FC = () => {
@@ -10,7 +11,7 @@ const Layout: React.FC = () => {
         <Wrapper>
           <MenuBar />
           <Main />
-          {/* <Sidebar /> */}
+          <Sidebar />
         </Wrapper>
       </Container>
     </>
